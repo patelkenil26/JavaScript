e0b153f6408a5f1e90f85e58b1976c42109c9d22
patelkenil26 <patelkenil2353@gmail.com>
2024-12-10 17:48:54 +0530

    add variable and dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const  accountId = "1234"
+let accountEmail = "kenil@gmail.com"
+var accountPassword = "123456"
+accountType = "Current"
+let accountState;
+
+// accountId = "123456"
+
+// console.log(accountId)
+// so in js cannot change const variable
+
+accountEmail = "kenilpatel@gmail.com"
+accountPassword = "57495"
+accountType = "Fixed"
+
+
+
+console.table([accountEmail,accountPassword,accountType,accountState])
+
+// In js var is block or functional scope
+
+// so in js if variable cannot assign value so give undefined on output screen 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+// code redability shoud be high
+let name = "Kenil"
+let age = 18
+let isLoggined = false
+
+// number => 2 to power 53
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+
+// Object
+
+console.log(typeof undefined) //aano type undefined j chhe
+console.log(typeof null) // aano type object chhe
